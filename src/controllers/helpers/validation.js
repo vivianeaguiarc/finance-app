047fbcd24899f7250ccd3dd9787a4309ae425147
viviane <vivianeaguiarc@outlook.com>
2025-09-05 14:45:08 -0300

    refactor: create generic validation helper functions file

---
+++ src/controllers/helpers/validation.js
@@ -0,0 +1,5 @@
+import validator from 'validator'
+import { badRequest } from './http.js'
+export const checkIfIdIsValid = (id) => validator.isUUID(id)
+
+export const invalidIdResponse = () => badRequest({ message: 'The provided ID is not valid' })
