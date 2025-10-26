688fbf65ab24e2671f3f1140d8bc9bf80e4435d8
Viviane <vivianeaguiarc@outlook.com>
2025-10-26 13:09:46 -0300

    feat: add Get User By Id Controller and validations

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import 'dotenv/config'
 import express from 'express'
 import { CreateUserController } from './src/controllers/create-user.js'
+import { GetUserByIdController } from './src/controllers/get-user-by-id.js'
 
 const app = express()
 app.use(express.json())
@@ -10,7 +11,11 @@ app.post('/api/users', async (request, response) => {
     const { statusCode, body } = await createUserController.execute(request)
     response.status(statusCode).send(body)
 })
-
+app.get('/api/users/:userId', async (request, response) => {
+    const gettUserByIdController = new GetUserByIdController()
+    const { statusCode, body } = await gettUserByIdController.execute(request)
+    response.status(statusCode).send(body)
+})
 app.listen(3000, () => {
     console.log(`Server running on port ${process.env.PORT || 3000}`)
 })
--- src/use-cases/get-user-by-id.js
+++ src/use-cases/get-user-by-id.js
@@ -1,7 +1,7 @@
 import { PostgresGetUserByIdRepository } from '../repositories/postgres/get-user-by-id.js'
 
 export class GetUserByIdUseCase {
-    async exceute(userId) {
+    async execute(userId) {
         const getUserByIdRepository = new PostgresGetUserByIdRepository()
         const user = await getUserByIdRepository.execute(userId)
         return user
