96ca3432044483d4d710605ea2f728b4a9e0a1ef
Viviane Aguiar <vivianeaguiarc@outlook.com>
2025-10-30 14:39:57 -0300

    refactor: move user controller to its own folder

--- src/controllers/user/create-user.js
+++ src/controllers/user/create-user.js
@@ -1,4 +1,4 @@
-import { EmailAlreadyInUseError } from '../errors/user.js'
+import { EmailAlreadyInUseError } from '../../errors/user.js'
 import {
     checkIfEmailIsValid,
     emailIsAlreadyInUseResponse,
@@ -7,7 +7,7 @@ import {
     serverError,
     created,
     checkIfPasswordIsValid,
-} from './helpers/index.js'
+} from '../helpers/index.js'
 export class CreateUserController {
     constructor(createUserUseCase) {
         this.createUserUseCase = createUserUseCase
