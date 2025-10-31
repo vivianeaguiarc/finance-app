475b88123c0f6062e520ff4bbdcd15feb507f061
Viviane Aguiar <vivianeaguiarc@outlook.com>
2025-10-31 16:27:02 -0300

    feat: add GettransactionByUserIdUseCase

---
+++ src/use-cases/transaction/get-transactions-by-user-id.js
@@ -0,0 +1,18 @@
+import { UserNotFoundError } from '../../errors/user'
+
+export class GetTransactionsByUserId {
+    constructor(getTransactionsByUserIdRepository, getUserByIdRepository) {
+        this.getTransactionsByUserIdRepository =
+            getTransactionsByUserIdRepository
+        this.getUserByIdRepository = getUserByIdRepository
+    }
+    async execute(params) {
+        const user = await this.getUserByIdRepository.execute(params.userId)
+        if (!user) {
+            return UserNotFoundError()
+        }
+        const transactions =
+            await this.getTransactionsByUserIdRepository.execute(params.userId)
+        return transactions
+    }
+}
