32a63f82824a5daa625e848ca106ebe1761e2791
Viviane Aguiar <vivianeaguiarc@outlook.com>
2025-10-31 19:36:20 -0300

    feat: add PostgresDeleteTransaction

---
+++ src/repositories/postgres/transaction/delete-transaction.js
@@ -0,0 +1,11 @@
+import { PostgresHelper } from '../../../db/postgres/helper.js'
+
+export class PostgresDeleteTransactionRepository {
+    async execute(transactionId) {
+        const transaction = await PostgresHelper.query(
+            'DELETE FROM transactions WHERE id = $1 RETURNING *',
+            [transactionId],
+        )
+        return transaction[0]
+    }
+}
