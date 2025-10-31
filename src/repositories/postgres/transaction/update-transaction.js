0b3714ee8f34da2af936821e11406a82380e0e32
Viviane Aguiar <vivianeaguiarc@outlook.com>
2025-10-31 18:41:48 -0300

    feat: add PostgresUpdateTransactionRepository

---
+++ src/repositories/postgres/transaction/update-transaction.js
@@ -0,0 +1,25 @@
+import { PostgresHelper } from '../../../db/postgres/helper.js'
+
+export class PostgresUpdateTransactionRepository {
+    async execute(userId, updateTransactionParams) {
+        const updateFields = []
+        const updateValues = []
+
+        Object.keys(updateTransactionParams).forEach((key) => {
+            updateFields.push(`${key} = $${updateFields.length + 1}`)
+            updateValues.push(updateTransactionParams[key])
+        })
+        updateValues.push(userId)
+        const updateQuery = `
+            UPDATE transactions
+            SET ${updateFields.join(', ')}
+            WHERE id = $${updateValues.length}
+            RETURNING *
+        `
+        const updateTransaction = await PostgresHelper.query(
+            updateQuery,
+            updateValues,
+        )
+        return updateTransaction[0]
+    }
+}
