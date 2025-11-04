d6b5e5f881e172716d2d48a347282f2c19017e2b
Viviane <vivianeaguiarc@outlook.com>
2025-11-04 17:24:13 -0300

    test: ensure 400 is returned if last_name is not provided

--- src/controllers/user/create-user.test.js
+++ src/controllers/user/create-user.test.js
@@ -19,7 +19,32 @@ describe('Create User Controller', () => {
         }
         const result = await createUserController.execute(httpRequest)
         expect(result.statusCode).toBe(201)
-        expect(result.body).not.toBeUndefined()
-        expect(result.body).not.toBeNull()
+        expect(result.body).toBe(httpRequest.body)
+    })
+    it('should return 400 if first_name is not provided', async () => {
+        const createUserUseCase = new CreateUserUseCaseStub()
+        const createUserController = new CreateUserController(createUserUseCase)
+        const httpRequest = {
+            body: {
+                last_name: 'Silva',
+                email: 'viviane@zmail.com',
+                password: 'secure1234',
+            },
+        }
+        const result = await createUserController.execute(httpRequest)
+        expect(result.statusCode).toBe(400)
+    })
+    it('should return 400 if last_name is not provided', async () => {
+        const createUserUseCase = new CreateUserUseCaseStub()
+        const createUserController = new CreateUserController(createUserUseCase)
+        const httpRequest = {
+            body: {
+                last_name: 'Aguiar',
+                email: 'viviane@zmail.com',
+                password: 'secure1234',
+            },
+        }
+        const result = await createUserController.execute(httpRequest)
+        expect(result.statusCode).toBe(400)
     })
 })
