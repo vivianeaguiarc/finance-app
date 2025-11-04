aa638f5fdd1e93ddfba9ab6b8e52dd24a9543bd6
Viviane <vivianeaguiarc@outlook.com>
2025-11-04 17:39:40 -0300

    test: ensure 400 is returned if email is not valid

--- src/controllers/user/create-user.test.js
+++ src/controllers/user/create-user.test.js
@@ -47,4 +47,30 @@ describe('Create User Controller', () => {
         const result = await createUserController.execute(httpRequest)
         expect(result.statusCode).toBe(400)
     })
+    it('should return 400 if email is not provided', async () => {
+        const createUserUseCase = new CreateUserUseCaseStub()
+        const createUserController = new CreateUserController(createUserUseCase)
+        const httpRequest = {
+            body: {
+                first_name: 'Viviane',
+                last_name: 'Silva',
+                password: 'secure1234',
+            },
+        }
+        const result = await createUserController.execute(httpRequest)
+        expect(result.statusCode).toBe(400)
+    })
+    it('should return 400 if password is not provided', async () => {
+        const createUserUseCase = new CreateUserUseCaseStub()
+        const createUserController = new CreateUserController(createUserUseCase)
+        const httpRequest = {
+            body: {
+                first_name: 'Viviane',
+                last_name: 'Silva',
+                email: 'viviane@zmail.com',
+            },
+        }
+        const result = await createUserController.execute(httpRequest)
+        expect(result.statusCode).toBe(400)
+    })
 })
