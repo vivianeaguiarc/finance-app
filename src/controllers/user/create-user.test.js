b9c747e70efcff23a8256c99cc1995861eb6e11c
Viviane <vivianeaguiarc@outlook.com>
2025-11-05 10:31:42 -0300

    test: add base http request to CreateUserController tests

--- src/controllers/user/create-user.test.js
+++ src/controllers/user/create-user.test.js
@@ -16,107 +16,66 @@ describe('Create User Controller', () => {
             createUserUseCase,
         }
     }
+    const httpRequest = {
+        body: {
+            first_name: faker.person.firstName(),
+            last_name: faker.person.lastName(),
+            email: faker.internet.email(),
+            password: faker.internet.password({ length: 7 }),
+        },
+    }
     it('should return 201 when creating a user successfully', async () => {
         const { sut } = makeSut()
-        const httpRequest = {
-            body: {
-                first_name: faker.person.firstName(),
-                last_name: faker.person.lastName(),
-                email: faker.internet.email(),
-                password: faker.internet.password(),
-            },
-        }
         const result = await sut.execute(httpRequest)
         expect(result.statusCode).toBe(201)
         expect(result.body).toEqual(httpRequest.body)
     })
     it('should return 400 if first_name is not provided', async () => {
         const { sut } = makeSut()
-        const httpRequest = {
-            body: {
-                last_name: faker.person.lastName(),
-                email: faker.internet.email(),
-                password: faker.internet.password(),
-            },
-        }
-        const result = await sut.execute(httpRequest)
+        const result = await sut.execute({
+            body: { ...httpRequest.body, first_name: undefined },
+        })
         expect(result.statusCode).toBe(400)
     })
     it('should return 400 if last_name is not provided', async () => {
         const { sut } = makeSut()
-        const httpRequest = {
-            body: {
-                last_name: faker.person.lastName(),
-                email: faker.internet.email(),
-                password: faker.internet.password(),
-            },
-        }
-        const result = await sut.execute(httpRequest)
+        const result = await sut.execute({
+            body: { ...httpRequest.body, last_name: undefined },
+        })
         expect(result.statusCode).toBe(400)
     })
     it('should return 400 if email is not provided', async () => {
         const { sut } = makeSut()
-        const httpRequest = {
-            body: {
-                first_name: faker.person.firstName(),
-                last_name: faker.person.lastName(),
-                email: 'invalid-email',
-                password: faker.internet.password(),
-            },
-        }
-        const result = await sut.execute(httpRequest)
+        const result = await sut.execute({
+            body: { ...httpRequest.body, email: 'invalid_email' },
+        })
         expect(result.statusCode).toBe(400)
     })
     it('should return 400 if password is not provided', async () => {
         const { sut } = makeSut()
-        const httpRequest = {
-            body: {
-                first_name: faker.person.firstName(),
-                last_name: faker.person.lastName(),
-                email: faker.internet.email(),
-                password: faker.internet.password({ length: 3 }),
-            },
-        }
-        const result = await sut.execute(httpRequest)
+        const result = await sut.execute({
+            body: { ...httpRequest.body, password: undefined },
+        })
         expect(result.statusCode).toBe(400)
     })
     it('should return 400 if password length is less than 6 characters', async () => {
         const { sut } = makeSut()
-        const httpRequest = {
+        const result = await sut.execute({
             body: {
-                first_name: faker.person.firstName(),
-                last_name: faker.person.lastName(),
-                email: faker.internet.email(),
-                password: faker.internet.password({ length: 3 }),
+                ...httpRequest.body,
+                password: faker.internet.password({ length: 5 }),
             },
-        }
-        const result = await sut.execute(httpRequest)
+        })
         expect(result.statusCode).toBe(400)
     })
     it('should call CreateUserUseCase with correct params', async () => {
         const { sut, createUserUseCase } = makeSut()
-        const httpRequest = {
-            body: {
-                first_name: faker.person.firstName(),
-                last_name: faker.person.lastName(),
-                email: faker.internet.email(),
-                password: faker.internet.password({ length: 7 }),
-            },
-        }
         const executeSpy = jest.spyOn(createUserUseCase, 'execute')
         await sut.execute(httpRequest)
         expect(executeSpy).toHaveBeenCalledWith(httpRequest.body)
     })
     it('should return 500 if CreateUserUseCase throws', async () => {
         const { sut, createUserUseCase } = makeSut()
-        const httpRequest = {
-            body: {
-                first_name: faker.person.firstName(),
-                last_name: faker.person.lastName(),
-                email: faker.internet.email(),
-                password: faker.internet.password({ length: 7 }),
-            },
-        }
         const executeSpy = jest
             .spyOn(createUserUseCase, 'execute')
             .mockImplementationOnce(() => {
@@ -129,14 +88,6 @@ describe('Create User Controller', () => {
     })
     it('should return 500 if CreateUserUseCase throws EmailIsAlreadyInUse error', async () => {
         const { sut, createUserUseCase } = makeSut()
-        const httpRequest = {
-            body: {
-                first_name: faker.person.firstName(),
-                last_name: faker.person.lastName(),
-                email: faker.internet.email(),
-                password: faker.internet.password({ length: 7 }),
-            },
-        }
         jest.spyOn(createUserUseCase, 'execute').mockImplementationOnce(() => {
             throw new EmailAlreadyInUseError(httpRequest.body.email)
         })
