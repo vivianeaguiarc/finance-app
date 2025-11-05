3130fcbee61d991d78aa4a507aaf5c19ebe623e2
Viviane <vivianeaguiarc@outlook.com>
2025-11-05 19:13:02 -0300

    test: ensure getUserByIdController calls getUserByIdUseCase with correct params

--- src/controllers/user/get-user-by-id.test.js
+++ src/controllers/user/get-user-by-id.test.js
@@ -21,24 +21,27 @@ describe('getUserById Controller', () => {
             getUserByIdUseCase,
         }
     }
+    const baseHttpRequest = {
+        params: {
+            userId: faker.string.uuid(),
+        },
+    }
     it('should return 200 if a user is found', async () => {
         const { sut } = makeSut()
-        const result = await sut.execute({
-            params: { userId: faker.string.uuid() },
-        })
+        const result = await sut.execute(baseHttpRequest)
         expect(result.statusCode).toBe(200)
     })
     it('should return 400 if id is invalid', async () => {
         const { sut } = makeSut()
-        const result = await sut.execute({ params: { userId: 'invalid_id' } })
+        const result = await sut.execute({
+            params: { userId: 'invalid_id' },
+        })
         expect(result.statusCode).toBe(400)
     })
     it('should return 404 if user is not found', async () => {
         const { sut, getUserByIdUseCase } = makeSut()
         jest.spyOn(getUserByIdUseCase, 'execute').mockResolvedValueOnce(null)
-        const result = await sut.execute({
-            params: { userId: faker.string.uuid() },
-        })
+        const result = await sut.execute(baseHttpRequest)
         expect(result.statusCode).toBe(404)
     })
 
@@ -47,9 +50,13 @@ describe('getUserById Controller', () => {
         jest.spyOn(getUserByIdUseCase, 'execute').mockRejectedValueOnce(
             new Error(),
         )
-        const result = await sut.execute({
-            params: { userId: faker.string.uuid() },
-        })
+        const result = await sut.execute(baseHttpRequest)
         expect(result.statusCode).toBe(500)
+    })
+    it('should call GetUserByIdUseCase with correct values', async () => {
+        const { sut, getUserByIdUseCase } = makeSut()
+        const executeSpy = jest.spyOn(getUserByIdUseCase, 'execute')
+        await sut.execute(baseHttpRequest)
+        expect(executeSpy).toHaveBeenCalledWith(baseHttpRequest.params.userId)
     })
 })
