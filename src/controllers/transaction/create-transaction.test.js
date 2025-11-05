37c8394a3d1e54e37f72082b9e036314d14d4964
Viviane <vivianeaguiarc@outlook.com>
2025-11-05 15:26:04 -0300

    test: ensure UpdateUserController return 400 when missing type

--- src/controllers/transaction/create-transaction.test.js
+++ src/controllers/transaction/create-transaction.test.js
@@ -65,4 +65,11 @@ describe('Create TransactionController', () => {
         })
         expect(response.statusCode).toBe(400)
     })
+    it('should return 400 when missing type', async () => {
+        const { sut } = makeSut()
+        const response = await sut.execute({
+            body: { ...baseHttpRequest.body, type: undefined },
+        })
+        expect(response.statusCode).toBe(400)
+    })
 })
