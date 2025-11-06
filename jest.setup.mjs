d5997f0fe740c82bfc885b1e195e5dbb9a4b5e46
Viviane <vivianeaguiarc@outlook.com>
2025-11-06 10:23:36 -0300

    chore: add test:coverage script

--- jest.setup.mjs
+++ jest.setup.mjs
@@ -1,2 +1,10 @@
+// jest.setup.mjs
 import { jest } from '@jest/globals'
 globalThis.jest = jest
+beforeAll(() => {
+    jest.spyOn(console, 'error').mockImplementation(() => {})
+})
+
+afterAll(() => {
+    console.error.mockRestore?.()
+})
