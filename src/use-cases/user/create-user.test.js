d0f23dfb3aae0a40d5a9689282df1f606dd3fa38
Viviane <vivianeaguiarc@outlook.com>
2025-11-06 14:49:25 -0300

    test: ensure CreateUserUseCase throws if GetUserByEmailRepository throws

--- src/use-cases/user/create-user.test.js
+++ src/use-cases/user/create-user.test.js
@@ -106,4 +106,13 @@ describe('CreateUserUseCase', () => {
             id: 'generated_id',
         })
     })
+    it('should throw if GetUserByEmailRepository throws', async () => {
+        const { sut, getUserByEmailRepository, user } = makeSut()
+        jest.spyOn(getUserByEmailRepository, 'execute').mockRejectedValueOnce(
+            new Error(),
+        )
+
+        const promise = sut.execute(user)
+        await expect(promise).rejects.toThrow()
+    })
 })
