e0f90a9aec0b18018954f9dd1a96813799ba3265
Viviane <vivianeaguiarc@outlook.com>
2025-11-06 15:05:37 -0300

    test: ensure DeleteUserUseCase calls DeleteUserReposirory with correct params

---
+++ src/use-cases/user/delete-user.test.js
@@ -0,0 +1,35 @@
+import { faker } from '@faker-js/faker'
+import { DeleteUserUseCase } from './delete-user.js'
+
+describe('DeleteUserUseCase', () => {
+    const user = {
+        id: faker.string.uuid(),
+        first_name: faker.person.firstName(),
+        last_name: faker.person.lastName(),
+        email: faker.internet.email(),
+        password: faker.internet.password({ length: 7 }),
+    }
+    class DeleteUserRepositoryStub {
+        async execute() {
+            return user
+        }
+    }
+    const makeSut = () => {
+        const deleteUserRepository = new DeleteUserRepositoryStub()
+        const sut = new DeleteUserUseCase(deleteUserRepository)
+        return { sut, deleteUserRepository }
+    }
+
+    it('should successfully delete a user', async () => {
+        const { sut } = makeSut()
+        const deletedUser = await sut.execute(faker.string.uuid())
+        expect(deletedUser).toEqual(user)
+    })
+    it('should call DeleteUserRepository with correct values', async () => {
+        const { sut, deleteUserRepository } = makeSut()
+        const deleteSpy = jest.spyOn(deleteUserRepository, 'execute')
+        const userId = faker.string.uuid()
+        await sut.execute(userId)
+        expect(deleteSpy).toHaveBeenCalledWith(userId)
+    })
+})
