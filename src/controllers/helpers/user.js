dd9e6c2e50b7ace97a8be238fb249bc777544355
Viviane <vivianeaguiarc@outlook.com>
2025-11-06 13:06:09 -0300

    refactor: remove unused code

--- src/controllers/helpers/user.js
+++ src/controllers/helpers/user.js
@@ -1,17 +1,4 @@
-import { badRequest, notFound } from './http.js'
-
-export const invalidPasswordResponse = () => {
-    return badRequest({
-        message: 'Password must be at least 6 characters long.',
-    })
-}
-
-export const emailIsAlreadyInUseResponse = () => {
-    return badRequest({
-        message:
-            'The provided email is already in use. Please use a different email address.',
-    })
-}
+import { notFound } from './http.js'
 
 export const userNotFoundResponse = () => {
     return notFound({
