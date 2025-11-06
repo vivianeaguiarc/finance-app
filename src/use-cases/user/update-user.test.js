587bcd8384ddc4b4eacee00f6c3ee293c774c44b
Viviane <vivianeaguiarc@outlook.com>
2025-11-06 18:17:53 -0300

    test: ensure UpdateUserUseCase throws if GetUserByEmailRepository throws

--- src/use-cases/user/update-user.test.js
+++ src/use-cases/user/update-user.test.js
@@ -40,6 +40,7 @@ describe('UpdateUserUseCase', () => {
             getUserByEmailRepository,
             passwordHasherAdapter,
             updateUserRepository,
+            user,
         }
     }
     it('should update user successfully (without email and password)', async () => {
@@ -86,5 +87,32 @@ describe('UpdateUserUseCase', () => {
         await expect(promise).rejects.toThrow(
             new EmailAlreadyInUseError(user.email),
         )
+    })
+    it('should call UpdateUserRepository with hashed password when provided', async () => {
+        const { sut, updateUserRepository, user } = makeSut()
+        const spy = jest.spyOn(updateUserRepository, 'execute')
+        const updateParams = {
+            first_name: faker.person.firstName(),
+            last_name: faker.person.lastName(),
+            email: user.email,
+            password: user.password,
+        }
+
+        await sut.execute(user.id, updateParams)
+
+        expect(spy).toHaveBeenCalledWith(user.id, {
+            ...updateParams,
+            password: 'hashed_password',
+        })
+    })
+    it('should throw if GetUserByEmailRepository throws', async () => {
+        const { sut, getUserByEmailRepository } = makeSut()
+        jest.spyOn(getUserByEmailRepository, 'execute').mockRejectedValueOnce(
+            new Error(),
+        )
+        const promise = sut.execute(faker.string.uuid(), {
+            email: faker.internet.email(),
+        })
+        await expect(promise).rejects.toThrow()
     })
 })
