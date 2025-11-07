a8990f32983ec0d66906f8581f50c3d3c205496b
Viviane <vivianeaguiarc@outlook.com>
2025-11-07 13:43:03 -0300

    chore: add test db to docker compose file

---
+++ src/repositories/postgres/user/create-user.test.js
@@ -0,0 +1,10 @@
+import { user } from '../../../tests/fixtures/user'
+import { PostgresCreateUserRepository } from './create-user'
+
+describe('CreateUserRepository', () => {
+    it('should create a user on db', async () => {
+        const sut = new PostgresCreateUserRepository()
+        const result = await sut.execute(user)
+        expect(result).not.toBeNull()
+    })
+})
