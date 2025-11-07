3f2eecb49ddeae544c025504e1e56cb76b1f8df3
Viviane <vivianeaguiarc@outlook.com>
2025-11-07 16:19:53 -0300

    test: ensure PostgresCreateUserRepository creates a user on db

--- src/repositories/postgres/user/create-user.test.js
+++ src/repositories/postgres/user/create-user.test.js
@@ -5,6 +5,8 @@ describe('CreateUserRepository', () => {
     it('should create a user on db', async () => {
         const sut = new PostgresCreateUserRepository()
         const result = await sut.execute(user)
-        expect(result).toBeTruthy()
+        expect(result.id).toBe(user.id)
+        expect(result.name).toBe(user.name)
+        expect(result.email).toBe(user.email)
     })
 })
