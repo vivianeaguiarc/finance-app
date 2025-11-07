d62c0abf7e92e951bb2948d714b59e691888fc76
Viviane <vivianeaguiarc@outlook.com>
2025-11-07 11:41:04 -0300

    feat: add user balance fixture

--- src/tests/fixtures/user.js
+++ src/tests/fixtures/user.js
@@ -7,3 +7,10 @@ export const user = {
     email: faker.internet.email(),
     password: faker.internet.password({ length: 7 }),
 }
+
+export const userBalance = {
+    earnings: faker.finance.amount(),
+    expenses: faker.finance.amount(),
+    investments: faker.finance.amount(),
+    balance: faker.finance.amount(),
+}
