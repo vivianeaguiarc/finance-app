b4e4c9b6f0488efd8f7f93066c2bc49e5729d9db
Viviane <vivianeaguiarc@outlook.com>
2025-11-09 10:22:31 -0300

    test: ensure makeUpdateTransactionController returns valid UpdateTransactionController and delete

--- src/factories/controllers/transaction.test.js
+++ src/factories/controllers/transaction.test.js
@@ -1,10 +1,24 @@
 import { CreateTransactionController } from '../../controllers/transaction/create-transaction.js'
 import { makeCreateTransactionController } from './transaction.js'
+import { UpdateTransactionController } from '../../controllers/transaction/update-transaction.js'
+import { makeUpdateTransactionController } from './transaction.js'
+import { DeleteTransactionController } from '../../controllers/transaction/delete-transaction.js'
+import { makeDeleteTransactionController } from './transaction.js'
 
 describe('Transaction Controller Factory', () => {
     it('should return a valid CreateTransactionController instance', () => {
         expect(makeCreateTransactionController()).toBeInstanceOf(
             CreateTransactionController,
+        )
+    })
+    it('should return a valid UpdateTransactionController instance', () => {
+        expect(makeUpdateTransactionController()).toBeInstanceOf(
+            UpdateTransactionController,
+        )
+    })
+    it('should return a valid DeleteTransactionController instance', () => {
+        expect(makeDeleteTransactionController()).toBeInstanceOf(
+            DeleteTransactionController,
         )
     })
 })
