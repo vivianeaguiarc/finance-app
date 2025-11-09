b85da89d565bf8cf58d79d37a360065c1e887da8
Viviane <vivianeaguiarc@outlook.com>
2025-11-09 10:33:57 -0300

    test: ensure makeGetTransactionByUserIdController returns valid GetTransactionByUserIdController

--- src/factories/controllers/transaction.test.js
+++ src/factories/controllers/transaction.test.js
@@ -1,9 +1,15 @@
 import { CreateTransactionController } from '../../controllers/transaction/create-transaction.js'
 import { makeCreateTransactionController } from './transaction.js'
+
 import { UpdateTransactionController } from '../../controllers/transaction/update-transaction.js'
 import { makeUpdateTransactionController } from './transaction.js'
+
 import { DeleteTransactionController } from '../../controllers/transaction/delete-transaction.js'
 import { makeDeleteTransactionController } from './transaction.js'
+
+// ✅ alinhar nome do arquivo e dos símbolos
+import { GetTransactionByUserIdController } from '../../controllers/transaction/get-transaction-by-user-id.js'
+import { makeGetTransactionsByUserIdController } from './transaction.js'
 
 describe('Transaction Controller Factory', () => {
     it('should return a valid CreateTransactionController instance', () => {
@@ -11,14 +17,22 @@ describe('Transaction Controller Factory', () => {
             CreateTransactionController,
         )
     })
+
     it('should return a valid UpdateTransactionController instance', () => {
         expect(makeUpdateTransactionController()).toBeInstanceOf(
             UpdateTransactionController,
         )
     })
+
     it('should return a valid DeleteTransactionController instance', () => {
         expect(makeDeleteTransactionController()).toBeInstanceOf(
             DeleteTransactionController,
+        )
+    })
+
+    it('should return a valid GetTransactionByUserIdController instance', () => {
+        expect(makeGetTransactionsByUserIdController()).toBeInstanceOf(
+            GetTransactionByUserIdController,
         )
     })
 })
