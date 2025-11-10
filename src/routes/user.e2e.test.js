2f793e7715947b1bd1ea9320224355f50e6ae09d
Viviane Aguiar <SEU_EMAIL_DO_GITHUB>
2025-11-10 11:45:25 -0300

    test: ensure PATCH /api/users/:userId returns 200 when user is updated

--- src/routes/user.e2e.test.js
+++ src/routes/user.e2e.test.js
@@ -1,6 +1,7 @@
 import request from 'supertest'
 import { app } from '../app.js'
 import { user } from '../tests/fixtures/index.js'
+import { faker } from '@faker-js/faker'
 
 describe(`User Routes E2E Tests`, () => {
     it('POST/users should return 201 when user is created', async () => {
@@ -24,5 +25,35 @@ describe(`User Routes E2E Tests`, () => {
 
         expect(response.status).toBe(200)
         expect(response.body).toEqual(createdUser)
+    })
+    it('PATCH /api/users/:userId should return 200 when user is updated', async () => {
+        // cria o usuário
+        const createRes = await request(app)
+            .post('/api/users')
+            .send({ ...user, id: undefined })
+
+        const createdUser = createRes.body
+
+        const updateUserParams = {
+            first_name: faker.person.firstName(),
+            last_name: faker.person.lastName(),
+            email: faker.internet.email(),
+            password: faker.internet.password(),
+        }
+
+        // atualiza
+        const response = await request(app)
+            .patch(`/api/users/${createdUser.id}`)
+            .send(updateUserParams)
+
+        expect(response.status).toBe(200)
+
+        const updated = response.body
+        expect(updated.first_name).toBe(updateUserParams.first_name)
+        expect(updated.last_name).toBe(updateUserParams.last_name)
+        expect(updated.email).toBe(updateUserParams.email)
+
+        // a maioria das APIs não retorna 'password' no body
+        expect(updated).not.toBe('password')
     })
 })
