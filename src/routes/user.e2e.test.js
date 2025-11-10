dbfe0ea512b32c9a456bffdfbbf61fe4837adef8
Viviane Aguiar <SEU_EMAIL_DO_GITHUB>
2025-11-10 19:01:50 -0300

    test: ensure GET /api/users/:userId should return 404 when user is not found

--- src/routes/user.e2e.test.js
+++ src/routes/user.e2e.test.js
@@ -98,4 +98,10 @@ describe(`User Routes E2E Tests`, () => {
     //         balance: '6000',
     //     })
     // })
+    it('GET /api/users/:userId should return 404 when user is not found', async () => {
+        const response = await request(app).get(
+            `/api/users/${faker.string.uuid()}`,
+        )
+        expect(response.status).toBe(404)
+    })
 })
