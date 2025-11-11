b12cb80e717754288ed7c611e4fa3c957e70176a
Viviane Aguiar <SEU_EMAIL_DO_GITHUB>
2025-11-11 08:31:27 -0300

    test: ensure POST /api/transactions should return 201 when transaction is created

---
+++ src/routes/transaction.e2e.test.js
@@ -0,0 +1,20 @@
+import request from 'supertest'
+import { app } from '../app.js'
+import { transaction, user } from '../tests/fixtures/index.js'
+
+describe(`Transaction Routes E2E Tests`, () => {
+    it('POST /api/transactions should return 201 when transaction is created', async () => {
+        // cria o usuário
+        const { body: createdUser } = await request(app)
+            .post('/api/users')
+            .send({ ...user, id: undefined })
+        // cria a transação
+        const response = await request(app)
+            .post('/api/transactions')
+            .send({ ...transaction, user_id: createdUser.id, id: undefined })
+        expect(response.status).toBe(201)
+        expect(response.body.user_id).toBe(createdUser.id)
+        expect(response.body.amount).toBe(String(transaction.amount))
+        expect(response.body.type).toBe(transaction.type)
+    })
+})
