dbeae691d94cc8350b8dfbfc178078f21c863507
Viviane Aguiar <SEU_EMAIL_DO_GITHUB>
2025-11-11 08:16:07 -0300

    feat: ensure UpdateUserController returns 404 if UserNotFoundError

--- src/controllers/user/update-user.js
+++ src/controllers/user/update-user.js
@@ -1,4 +1,4 @@
-import { EmailAlreadyInUseError } from '../../errors/user.js'
+import { EmailAlreadyInUseError, UserNotFoundError } from '../../errors/user.js'
 import { updatedUserSchema } from '../../schemas/user.js'
 import {
     checkIfIdIsValid,
@@ -6,6 +6,7 @@ import {
     badRequest,
     ok,
     serverError,
+    userNotFoundResponse,
 } from '../helpers/index.js'
 import { ZodError } from 'zod'
 
@@ -39,6 +40,9 @@ export class UpdateUserController {
             }
             if (error instanceof EmailAlreadyInUseError) {
                 return badRequest({ message: error.message })
+            }
+            if (error instanceof UserNotFoundError) {
+                return userNotFoundResponse()
             }
             return serverError({
                 message: 'An unexpected error occurred.',
