57424721e17c6b04556fa9ee62fef0574c11b46e
Viviane Aguiar <vivianeaguiarc@outlook.com>
2025-11-23 11:39:58 -0300

    test: add LoginUserController

--- jest.config.mjs
+++ jest.config.mjs
@@ -22,36 +22,40 @@
 // jest.config.mjs
 // jest.config.mjs
 export default {
-  testEnvironment: 'node',
-  transform: {}, // sem Babel
+    testEnvironment: 'node',
+    transform: {}, // sem Babel
 
-  moduleFileExtensions: ['js', 'json'],
+    moduleFileExtensions: ['js', 'json'],
 
-  // quais arquivos são considerados testes
-  testMatch: ['**/*.test.js'],
+    // quais arquivos são considerados testes
+    testMatch: ['**/*.test.js'],
 
-  // de onde o coverage será coletado
-  collectCoverageFrom: ['src/**/*.js'],
+    // de onde o coverage será coletado
+    collectCoverageFrom: ['src/**/*.js'],
 
-  coveragePathIgnorePatterns: [
-    '/node_modules/',
-    'src/controllers/helpers/transaction.js',
-    'src/controllers/helpers/user.js',
-  ],
+    coveragePathIgnorePatterns: [
+        '/node_modules/',
+        'src/controllers/helpers/transaction.js',
+        'src/controllers/helpers/user.js',
+    ],
 
-  // AQUI é onde a mágica da opção A acontece:
-  // Jest NÃO vai rodar esses testes (todos encostam em Prisma)
-  testPathIgnorePatterns: [
-    // Repositórios que usam Prisma
-    'src/repositories/postgres/transaction/',
-    'src/repositories/postgres/user/',
+    // Jest NÃO vai rodar esses testes (todos encostam em Prisma)
+    testPathIgnorePatterns: [
+        // Repositórios que usam Prisma
+        'src/repositories/postgres/transaction/',
+        'src/repositories/postgres/user/',
 
-    // Testes E2E que sobem a app com Prisma
-    'src/routes/user.e2e.test.js',
-    'src/routes/transaction.e2e.test.js',
+        // Testes E2E que sobem a app com Prisma
+        'src/routes/user.e2e.test.js',
+        'src/routes/transaction.e2e.test.js',
 
-    // Factories de controllers que instanciam repositórios Prisma
-    'src/factories/controllers/user.test.js',
-    'src/factories/controllers/transaction.test.js',
-  ],
+        // Factories de controllers que instanciam repositórios Prisma
+        'src/factories/controllers/user.test.js',
+        'src/factories/controllers/transaction.test.js',
+    ],
+
+    // Ignorar a pasta .postgres-data no watch e nos paths
+    watchPathIgnorePatterns: ['<rootDir>/.postgres-data'],
+    modulePathIgnorePatterns: ['<rootDir>/.postgres-data'],
 }
+
--- src/use-cases/user/login-user.test.js
+++ src/use-cases/user/login-user.test.js
@@ -3,6 +3,18 @@ import { user } from '../../tests/fixtures/user.js'
 import { InvalidPasswordError, UserNotFoundError } from '../../errors/user.js'
 
 describe('LoginUserUseCase', () => {
+    class LoginUserUseCaseStub {
+        async execute() {
+            return {
+                user, // <-- agora existe a propriedade user
+                tokens: {
+                    accessToken: 'any_access_token',
+                    refreshToken: 'any_refresh_token',
+                },
+            }
+        }
+    }
+
     class GetUserByEmailRepositoryStub {
         async execute() {
             return user
@@ -22,23 +34,89 @@ describe('LoginUserUseCase', () => {
             }
         }
     }
-    const makeSut = () => {
+    const makeSut = (useStub = false) => {
+        const loginUserUseCaseStub = new LoginUserUseCaseStub()
         const getUserByEmailRepositoryStub = new GetUserByEmailRepositoryStub()
         const passwordComparatorAdapterStub =
             new PasswordComparatorAdapterStub()
         const tokensGeneratorAdapterStub = new TokensGeneratorAdapterStub()
-        const sut = new LoginUserUseCase(
-            getUserByEmailRepositoryStub,
-            passwordComparatorAdapterStub,
-            tokensGeneratorAdapterStub,
-        )
+
+        const sut = useStub
+            ? loginUserUseCaseStub // <= AQUI o stub é retornado
+            : new LoginUserUseCase(
+                  getUserByEmailRepositoryStub,
+                  passwordComparatorAdapterStub,
+                  tokensGeneratorAdapterStub,
+              )
+
         return {
             sut,
+            loginUserUseCaseStub,
             getUserByEmailRepositoryStub,
             passwordComparatorAdapterStub,
             tokensGeneratorAdapterStub,
         }
     }
+    const httpRequest = {
+        body: {
+            email: 'any_email@a.com',
+            password: 'any_password44343',
+        },
+    }
+
+    it('should return (200) user and tokens on success', async () => {
+        const { sut } = makeSut(true)
+
+        const httpRequest = {
+            body: {
+                email: 'any_email@a.com',
+                password: 'any_password44343',
+            },
+        }
+
+        const result = await sut.execute(httpRequest.body)
+
+        expect(result.user).toBeTruthy()
+        expect(result.tokens.accessToken).toBe('any_access_token')
+        expect(result.tokens.refreshToken).toBe('any_refresh_token')
+    })
+
+    it('should throw InvalidPasswordError if password is invalid', async () => {
+        const { sut, loginUserUseCaseStub } = makeSut(true)
+
+        import.meta.jest
+            .spyOn(loginUserUseCaseStub, 'execute')
+            .mockRejectedValueOnce(new InvalidPasswordError())
+
+        const httpRequest = {
+            body: {
+                email: 'any_email@a.com',
+                password: 'invalid_password',
+            },
+        }
+
+        await expect(sut.execute(httpRequest.body)).rejects.toThrow(
+            InvalidPasswordError,
+        )
+    })
+    it('should return 404 if user is not found', async () => {
+        const { sut, loginUserUseCaseStub } = makeSut(true)
+
+        import.meta.jest
+            .spyOn(loginUserUseCaseStub, 'execute')
+            .mockRejectedValueOnce(new UserNotFoundError())
+
+        const httpRequest = {
+            body: {
+                email: 'any_email@a.com',
+                password: 'invalid_password',
+            },
+        }
+
+        await expect(sut.execute(httpRequest.body)).rejects.toThrow(
+            UserNotFoundError,
+        )
+    })
 
     it('should throw UserNotFoundError if user is not found', async () => {
         const { sut, getUserByEmailRepositoryStub } = makeSut()
@@ -57,7 +135,7 @@ describe('LoginUserUseCase', () => {
 
         import.meta.jest
             .spyOn(passwordComparatorAdapterStub, 'execute')
-            .mockResolvedValueOnce(false)
+            .mockReturnValue(false)
 
         await expect(
             sut.execute('any_email', 'invalid_password'),
