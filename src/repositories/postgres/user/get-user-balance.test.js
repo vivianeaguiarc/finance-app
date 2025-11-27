7aab8b0bad191618a9b569d7e2d7b39265c18d84
Viviane Aguiar <vivianeaguiarc@outlook.com>
2025-11-27 19:02:22 -0300

    test: ensure GetUserBalanceRepository receives from and to params

--- src/repositories/postgres/user/get-user-balance.test.js
+++ src/repositories/postgres/user/get-user-balance.test.js
@@ -5,6 +5,9 @@ import { PostgresGetUserBalanceRepository } from './get-user-balance.js'
 import { TransactionType } from '@prisma/client'
 
 describe('PostgresGetUserBalanceRepository', () => {
+    const from = '2023-01-01'
+    const to = '2023-12-31'
+
     it('should get user balance on db', async () => {
         let createdUserId
         await prisma.$transaction(async (tx) => {
@@ -15,35 +18,35 @@ describe('PostgresGetUserBalanceRepository', () => {
                     {
                         name: faker.string.sample(),
                         amount: 5000,
-                        date: faker.date.recent(),
+                        date: new Date(from),
                         type: 'EARNING',
                         user_id: user.id,
                     },
                     {
                         name: 'Freelance',
                         amount: 5000,
-                        date: faker.date.recent(),
+                        date: new Date(from),
                         type: 'EARNING',
                         user_id: user.id,
                     },
                     {
                         name: faker.string.sample(),
                         amount: 1000,
-                        date: faker.date.recent(),
+                        date: new Date(to),
                         type: 'EXPENSE',
                         user_id: user.id,
                     },
                     {
                         name: faker.string.sample(),
                         amount: 1000,
-                        date: faker.date.recent(),
+                        date: new Date(to),
                         type: 'EXPENSE',
                         user_id: user.id,
                     },
                     {
                         name: faker.string.sample(),
                         amount: 3000,
-                        date: faker.date.recent(),
+                        date: new Date(to),
                         type: 'INVESTMENT',
                         user_id: user.id,
                     },
@@ -54,8 +57,7 @@ describe('PostgresGetUserBalanceRepository', () => {
         })
 
         const sut = new PostgresGetUserBalanceRepository()
-        const result = await sut.execute(createdUserId)
-
+        const result = await sut.execute(createdUserId, from, to)
         expect(result.earnings.toString()).toBe('10000')
         expect(result.expenses.toString()).toBe('2000')
         expect(result.investments.toString()).toBe('3000')
@@ -67,12 +69,13 @@ describe('PostgresGetUserBalanceRepository', () => {
             prisma.transaction,
             'aggregate',
         )
-        await sut.execute(faker.string.uuid())
+        await sut.execute(faker.string.uuid(), from, to)
         expect(prismaSpy).toHaveBeenCalledTimes(3)
         expect(prismaSpy).toHaveBeenNthCalledWith(1, {
             where: {
                 user_id: expect.any(String),
                 type: TransactionType.EXPENSE,
+                date: { gte: new Date(from), lte: new Date(to) },
             },
             _sum: { amount: true },
         })
@@ -80,6 +83,7 @@ describe('PostgresGetUserBalanceRepository', () => {
             where: {
                 user_id: expect.any(String),
                 type: TransactionType.EARNING,
+                date: { gte: new Date(from), lte: new Date(to) },
             },
             _sum: { amount: true },
         })
@@ -87,8 +91,17 @@ describe('PostgresGetUserBalanceRepository', () => {
             where: {
                 user_id: expect.any(String),
                 type: TransactionType.INVESTMENT,
+                date: { gte: new Date(from), lte: new Date(to) },
             },
             _sum: { amount: true },
         })
     })
+})
+it('should throw if Prisma throws', async () => {
+    const sut = new PostgresGetUserBalanceRepository()
+    import.meta.jest
+        .spyOn(prisma.transaction, 'aggregate')
+        .mockRejectedValueOnce(new Error())
+    const promise = sut.execute(faker.string.uuid(), from, to)
+    await expect(promise).rejects.toThrow()
 })
