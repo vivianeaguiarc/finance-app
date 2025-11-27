03b77da93bb3296bb8e95c19cf4bcbe976b8acd8
Viviane Aguiar <vivianeaguiarc@outlook.com>
2025-11-27 18:51:32 -0300

    feat: ensure GetUserBalanaceUseCase receives from and to params

--- src/use-cases/user/get-user-balance.js
+++ src/use-cases/user/get-user-balance.js
@@ -5,12 +5,16 @@ export class GetUserBalanceUseCase {
         this.getUserBalanceRepository = getUserBalanceRepository
         this.getUserByIdRepository = getUserByIdRepository
     }
-    async execute(userId) {
+    async execute(userId, from, to) {
         const user = await this.getUserByIdRepository.execute(userId)
         if (!user) {
             throw new UserNotFoundError(userId)
         }
-        const balance = await this.getUserBalanceRepository.execute(userId)
+        const balance = await this.getUserBalanceRepository.execute(
+            userId,
+            from,
+            to,
+        )
         return balance
     }
 }
