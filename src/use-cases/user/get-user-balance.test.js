b1230f33c2500cc092a58da04a5d56849d414eac
Viviane Aguiar <vivianeaguiarc@outlook.com>
2025-11-27 18:51:48 -0300

    test: ensure GetUserBalanaceUseCase receives from and to params

--- src/use-cases/user/get-user-balance.test.js
+++ src/use-cases/user/get-user-balance.test.js
@@ -26,9 +26,12 @@ describe('GetUserBalanceUseCase', () => {
             getUserByIdRepository,
         }
     }
+    const from = '2023-01-01'
+    const to = '2023-12-31'
+
     it('should successfully get user balance', async () => {
         const { sut } = makeSut()
-        const result = await sut.execute(faker.string.uuid())
+        const result = await sut.execute(faker.string.uuid(), from, to)
         expect(result).toEqual(userBalance)
     })
 
@@ -39,20 +42,20 @@ describe('GetUserBalanceUseCase', () => {
             .mockResolvedValueOnce(null)
 
         const userId = faker.string.uuid()
-        const promise = sut.execute(userId)
+        const promise = sut.execute(userId, from, to)
 
         await expect(promise).rejects.toThrow(
             `User with id ${userId} not found.`,
         )
     })
-    it('should call GetUserByIdRepository with correct values', async () => {
+    it('should call GetUserByIdRepository with correct params', async () => {
         const { sut, getUserByIdRepository } = makeSut()
         const getUserByIdSpy = import.meta.jest.spyOn(
             getUserByIdRepository,
             'execute',
         )
         const userId = faker.string.uuid()
-        await sut.execute(userId)
+        await sut.execute(userId, from, to)
         expect(getUserByIdSpy).toHaveBeenCalledWith(userId)
     })
     it('should throw if GetUserBalanceRepository throws', async () => {
@@ -60,7 +63,7 @@ describe('GetUserBalanceUseCase', () => {
         import.meta.jest
             .spyOn(getUserBalanceRepository, 'execute')
             .mockRejectedValueOnce(new Error())
-        const promise = sut.execute(faker.string.uuid())
+        const promise = sut.execute(faker.string.uuid(), from, to)
         await expect(promise).rejects.toThrow()
     })
     it('should throw if GetUserByIdRepository throws', async () => {
@@ -68,7 +71,7 @@ describe('GetUserBalanceUseCase', () => {
         import.meta.jest
             .spyOn(getUserByIdRepository, 'execute')
             .mockRejectedValueOnce(new Error())
-        const promise = sut.execute(faker.string.uuid())
+        const promise = sut.execute(faker.string.uuid(), from, to)
         await expect(promise).rejects.toThrow()
     })
     it('should throw if GetUserBalanceRepository throws', async () => {
@@ -76,7 +79,7 @@ describe('GetUserBalanceUseCase', () => {
         import.meta.jest
             .spyOn(getUserBalanceRepository, 'execute')
             .mockRejectedValueOnce(new Error())
-        const promise = sut.execute(faker.string.uuid())
+        const promise = sut.execute(faker.string.uuid(), from, to)
         await expect(promise).rejects.toThrow()
     })
 })
