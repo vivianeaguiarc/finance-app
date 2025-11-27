ae09813576fbc3dcb25626f81c32716d63c368cd
Viviane Aguiar <vivianeaguiarc@outlook.com>
2025-11-27 15:59:54 -0300

    feat: ensure GetTransactionsByUserIdUseCase calls repository with from and to

--- src/use-cases/transaction/get-transaction-by-user-id.js
+++ src/use-cases/transaction/get-transaction-by-user-id.js
@@ -5,13 +5,17 @@ export class GetTransactionByUserIdUseCase {
         this.getTransactionByUserIdRepository = getTransactionByUserIdRepository
         this.getUserByIdRepository = getUserByIdRepository
     }
-    async execute(userId) {
+    async execute(userId, from, to) {
         const user = await this.getUserByIdRepository.execute(userId)
         if (!user) {
             throw new UserNotFoundError(userId)
         }
         const transactions =
-            await this.getTransactionByUserIdRepository.execute(userId)
+            await this.getTransactionByUserIdRepository.execute(
+                userId,
+                from,
+                to,
+            )
         return transactions
     }
 }
